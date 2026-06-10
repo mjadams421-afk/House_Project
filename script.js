e1b789608f6abb3afca639dfc9e6c16485ffeae0
Mat <mjadams421@gmail.com>
2026-06-10 20:55:03 +0000

    Added Changes

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ const responseA = document.getElementById('responseA');
   function AcresDisp() {
   const sqFeet = window.prompt('Enter property area');
   const acres = sqFeet*.00002296;
-  let AcresResp = ' Your propery is ' + sqFeet + 'and ';
-  if (acres < 1) { AcresResp += 'the acres is ' + acres + 'which is not enough for a typical suburban home.'}
-  else if(acres >= 1 && acres <= 3) { AcresResp += 'the acres is ' + acres + 'which is enough for a small yet generous suburban home.'}
-  else if (acres > 3 && acres <= 5) { AcresResp += 'the acres is ' + acres + 'which is enough for a large suburban home with a spacious yard.'}
-  else if (acres > 5 && acres <= 10) { AcresResp += 'the acres is ' + acres + 'which is enough for a small farm or a large estate.'}
-  else if (acres > 10 && acres <= 20) { AcresResp += 'the acres is ' + acres + 'which is enough for a medium-sized farm or a large estate with plenty of land.'}
-  else if (acres > 20) { AcresResp += 'the acres is ' + acres + 'which is enough for a large farm.'}
+  let AcresResp = ' Your propery is ' + sqFeet + ' SF and ';
+  if (acres < 1) { AcresResp += 'the acres is ' + acres + 'acres which is not enough for a typical suburban home.'}
+  else if(acres >= 1 && acres <= 3) { AcresResp += 'the acres is ' + acres + ' acres which is enough for a small yet generous suburban home.'}
+  else if (acres > 3 && acres <= 5) { AcresResp += 'the acres is ' + acres + ' acres which is enough for a large suburban home with a spacious yard.'}
+  else if (acres > 5 && acres <= 10) { AcresResp += 'the acres is ' + acres + ' acres which is enough for a small farm or a large estate.'}
+  else if (acres > 10 && acres <= 20) { AcresResp += 'the acres is ' + acres + ' acres which is enough for a medium-sized farm or a large estate with plenty of land.'}
+  else if (acres > 20) { AcresResp += 'the acres is ' + acres + ' acres which is enough for a large farm.'}
   else { AcresResp += 'the number is not valid to calculate acres.'}
   responseA.innerHTML = AcresResp;
   }
